926631a0b8add54633b1ee8e72e532a47a447c76
Your Name <you@example.com>
2025-05-13 19:57:40 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -3,73 +3,72 @@
 #include "list.h"
 
 typedef struct{
-  int sudo[9][9];
+	int sudo[9][9];
 } Node;
 
 Node *createNode(){
-  Node *n = (Node *)malloc(sizeof(Node));
-  return n;
+	Node *n = (Node *)malloc(sizeof(Node));
+	return n;
 }
 
 Node *copy(Node *n){
-  Node *new = (Node *)malloc(sizeof(Node));
-  *new = *n;
-  return new;
+	Node *new = (Node *)malloc(sizeof(Node));
+	*new = *n;
+	return new;
 }
 
 Node *read_file(char *file_name){
-  Node *n = createNode();
-  FILE *file = fopen(file_name, "r");
-  int i, j;
-  for (i = 0; i < 9; i++)  {
-    for (j = 0; j < 9; j++)    {
-      if (!fscanf(file, "%d", &n->sudo[i][j]))
-        printf("failed to read data!");
-    }
-  }
+	Node *n = createNode();
+	FILE *file = fopen(file_name, "r");
+	int i, j;
+	for (i = 0; i < 9; i++)	{
+		for (j = 0; j < 9; j++)		{
+			if (!fscanf(file, "%d", &n->sudo[i][j]))
+				printf("failed to read data!");
+		}
+	}
 
-  fclose(file);
-  return n;
+	fclose(file);
+	return n;
 }
 
 void print_node(Node *n){
-  int i, j;
-  for (i = 0; i < 9; i++)  {
-    for (j = 0; j < 9; j++)
-      printf("%d ", n->sudo[i][j]);
-    printf("\n");
-  }
-  printf("\n");
+	int i, j;
+	for (i = 0; i < 9; i++)	{
+		for (j = 0; j < 9; j++)
+			printf("%d ", n->sudo[i][j]);
+		printf("\n");
+	}
+	printf("\n");
 }
 
 int is_valid(Node *n){
 
-  return 1;
+	return 1;
 }
 
 List *get_adj_nodes(Node *n){
-  List *list = createList();
-  for(int i = 0; i < 9; i++){
-    for(int j = 0; j < 9; j++){
-      if(n->sudo[i][j] == 0){
-        for(int k = 1; k <= 9; k++){
-          Node * nuevo = copy(n);
-          nuevo->sudo[i][j] = k
-        }
-      }
-    }
+	List *list = createList();
+	for (int i = 0; i < 9; i++)	{
+		for (int j = 0; j < 9; j++){
+			if (n->sudo[i][j] == 0){
+				for (int k = 1; k <= 9; k++){
+					Node *nuevo = copy(n);
+					nuevo->sudo[i][j] = k;
+				}
+			}
+		}
+	}
 
-  } 
-
-  return list;
+	return list;
 }
 
 int is_final(Node *n){
-  return 0;
+	return 0;
 }
 
 Node *DFS(Node *initial, int *cont){
-  return NULL;
+	return NULL;
 }
 
 /*
